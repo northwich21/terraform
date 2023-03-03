f839980c3877a7f01c5d1fc0657ef054a92318bf
northwich21 <northwich21@msn.com>
2023-03-03 16:29:03 +0100

    Initial Commit

---
+++ providers.tf
@@ -0,0 +1,16 @@
+# We strongly recommend using the required_providers block to set the
+# Azure Provider source and version being used
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.0.0"
+    }
+  }
+}
+
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+  features {}
+  subscription_id = "a177ac1b-f0e1-45e5-a6c4-80266ff85e1d"
+}
